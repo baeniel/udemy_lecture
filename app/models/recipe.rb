12956863f50be46a68bb2275e53425bccbd72bcb
jbtrum <126jb@likelion.org>
2019-09-07 22:27:04 +0900

    m:n asscociation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,7 @@ class Recipe < ApplicationRecord
   validates :description, presence: true, length: { minimum: 5, maximum: 300 }
   belongs_to :chef, optional: true
   validates :chef_id, presence: true
+
+  has_many :recipe_ingredients, dependent: :destroy
+  has_many :ingredients, through: :recipe_ingredients
 end
